6593e92889f5e3ce73202abd690e0f8d4e85897c
Karamaz0V1 <florent.guiotte@gmail.com>
2014-11-02 23:41:48 +0100

    Fin tp4

--- tp/tp4.cpp
+++ tp/tp4.cpp
@@ -74,6 +74,25 @@ void histocumule(const vpImage<unsigned char> &I, unsigned int* histo, unsigned 
     }
 }
 
+void eqm(const vpImage<vpRGBa> &I1, const vpImage<vpRGBa> &I2){
+    // les 2 images font la même taille. I1 = image de base, I2 = image modifiée
+    // On doit obtenir 334 pour nbbits = 2
+    float EQMR=0;   
+    float EQMG=0;
+    float EQMB=0;
+    float EQM=0;
+    for (int i=0 ; i< I1.getHeight(); i++){
+        for (int j=0; j < I1.getWidth(); j++){
+            EQMR+=pow(I1[i][j].R - I2[i][j].R,2);
+            EQMG+=pow(I1[i][j].G - I2[i][j].G,2);
+            EQMB+=pow(I1[i][j].B - I2[i][j].B,2);
+        }   
+    }
+    EQM = ((EQMR+EQMG+EQMB)/3)/(I1.getHeight()*I1.getWidth());
+    cout << "EQM =" << EQM << endl;
+
+}
+
 void quantitatifnonuniformesurcomposante(const vpImage<unsigned char> &compo, unsigned char * corresCompo, int * classeCompo, int n) {
     cout<<"Travail sur une composante ---"<<endl;
     unsigned int histo[256];
@@ -132,6 +151,8 @@ void quantifnonuniforme(const vpImage<vpRGBa> &I, int n) {
         for(int j=0; j<I.getWidth(); j++)
             I2[i][j] = pal[corres(I[i][j], crClasse, cgClasse, cbClasse, n)];
 
+    eqm(I,I2);
+
     vpDisplayX d(I2,100,300);
     vpDisplay::setTitle(I2, "Tadaaaa!");
     vpDisplay::display(I2);
@@ -139,12 +160,8 @@ void quantifnonuniforme(const vpImage<vpRGBa> &I, int n) {
     vpDisplay::getClick(I2);
 }
 
-void quantifuniforme(const vpImage<vpRGBa> &I, const int tailleComposante) {
-    int n = tailleComposante;
-
-    int classe[256];
+void creer_palette_uni(vpRGBa * pal, int * classe, int n) {
     unsigned char rep[n];
-
     int pas = 256 / n;
     int palier = 0;
 
@@ -156,15 +173,22 @@ void quantifuniforme(const vpImage<vpRGBa> &I, const int tailleComposante) {
         }
         classe[i] = palier;
     }
-
-    vpRGBa pal[(int)pow(n,3)];
     creerPalette(rep, rep, rep, n, pal);
+}
+
+void quantifuniforme(const vpImage<vpRGBa> &I, const int tailleComposante) {
+
+    int classe[256];
+    vpRGBa pal[(int)pow(tailleComposante,3)];
+    creer_palette_uni(pal, classe, tailleComposante);
 
     vpImage<vpRGBa> I2(I.getHeight(),I.getWidth());
 
     for(int i=0; i<I.getHeight(); i++)
         for(int j=0; j<I.getWidth(); j++)
-            I2[i][j] = pal[corres(I[i][j], classe, classe, classe, n)];
+            I2[i][j] = pal[corres(I[i][j], classe, classe, classe, tailleComposante)];
+
+    eqm(I,I2);
 
     vpDisplayX da(I2,100,300);
     vpDisplay::setTitle(I2, "Tadaaaa!");
@@ -182,24 +206,59 @@ int distance_pix(const vpRGBa &a, const vpRGBa &b) {
 void quantifVectoriel(const vpImage<vpRGBa> &imasrc,vpImage<vpRGBa> &imadest, vpRGBa * pal,const int taillePalette) 
 {
     float erreur = 0;
-    int i = 0;
+    int ittr = 0;
 
     float seuilErreur = 0.1;
-    int seuilIteration = 100000;
+    int seuilIteration = 10;
 
+    int mapClasse[imasrc.getWidth()][imasrc.getHeight()];
+
+    int nbCompo[taillePalette];
     double sumCompo[taillePalette][3];
-    for (int i=0; i<taillePalette; i++) 
+    for (int i=0; i<taillePalette; i++) {
+        nbCompo[i] = 0;
         for (int j=0; j<3; j++) sumCompo[i][j] = 0;
+    }
 
-    while (erreur < seuilErreur && i < seuilIteration) {
+    while (erreur < seuilErreur && ittr < seuilIteration) {
         for(int i=0; i<imasrc.getHeight(); i++)
             for(int j=0; j<imasrc.getWidth(); j++) {
                 int dist_min = distance_pix(imasrc[i][j],pal[0]);
                 int classe = 0;
                 for(int x=1; x<taillePalette; x++)
                     if (distance_pix(imasrc[i][j],pal[x]) < dist_min) int classe = x;
+                sumCompo[classe][0]+=imasrc[i][j].R;
+                sumCompo[classe][1]+=imasrc[i][j].G;
+                sumCompo[classe][2]+=imasrc[i][j].B;
+                nbCompo[classe]++;
             }
+
+        for(int x=1; x<taillePalette; x++) {
+            pal[x].R = sumCompo[x][0] / nbCompo[x];
+            pal[x].G = sumCompo[x][1] / nbCompo[x];
+            pal[x].B = sumCompo[x][2] / nbCompo[x];
+        }
+
+        double sumErreur = 0;
+        for(int i=0; i<imasrc.getHeight(); i++)
+            for(int j=0; j<imasrc.getWidth(); j++)
+                sumErreur += distance_pix(imasrc[i][j],pal[mapClasse[i][j]]);
+        
+        erreur = sumErreur / (imasrc.getWidth()*imasrc.getHeight());
+        ittr++;
     }
+
+    for(int i=0; i<imasrc.getHeight(); i++)
+        for(int j=0; j<imasrc.getWidth(); j++)
+            imadest[i][j] = pal[mapClasse[i][j]];
+}
+
+void testQuantifVectoriel(const vpImage<vpRGBa> &imasrc, int n) {
+    vpImage<vpRGBa> ires(imasrc.getHeight(),imasrc.getWidth());
+    int classe[256];
+    vpRGBa pal[(int)pow(n,3)];
+    creer_palette_uni(pal, classe, n);
+    quantifVectoriel(imasrc, ires, pal, pow(n,3));
 }
 
 int main(int argc, char **argv)
@@ -220,12 +279,11 @@ int main(int argc, char **argv)
     cout <<"entrer le nombre bits par composante :"<<endl;
     cin >> nbbits;
 	
-	// creation du menu
     int tailleComposante = pow(2,nbbits);
 
     quantifuniforme(I,tailleComposante);
     quantifnonuniforme(I,tailleComposante);
-	
+	testQuantifVectoriel(I,tailleComposante);
 	
 	
 	vpDisplay::close(I) ;
