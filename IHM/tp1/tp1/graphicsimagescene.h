e5aa0481e94cfbab6ca5c16d163596c36d7f88eb
Fred <zokta@Arcktick.(none)>
2014-12-08 14:26:39 +0100

    TP IHM

---
+++ IHM/tp1/tp1/graphicsimagescene.h
@@ -0,0 +1,34 @@
+#ifndef GRAPHICSIMAGESCENE_H
+#define GRAPHICSIMAGESCENE_H
+
+#include <QGraphicsScene>
+#include <QMessageBox>
+#include <QGraphicsPixmapItem>
+#include <QFileDialog>
+#include <QImage>
+
+class GraphicsImageScene : public QGraphicsScene
+{
+    Q_OBJECT
+public:
+    GraphicsImageScene(QWidget *window,QObject *parent = 0);
+    virtual ~GraphicsImageScene();
+
+protected:
+    QImage m_image;
+private:
+    QWidget *m_window;
+
+public:
+    void setImage(const QImage &image);
+    QImage image() const;
+    
+signals:
+    
+public slots:
+    void printImage();
+    void saveImage();
+    
+};
+
+#endif // GRAPHICSIMAGESCENE_H
