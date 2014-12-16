c2b2af73842b4d5c57b897f2400c06de3a152608
Karamaz0v1 <florent.guiotte@gmail.com>
2014-12-16 18:15:45 +0100

    Segfault party ! Yeah !

--- TIA/tpFiltrage/src/tp_filtrage.c
+++ TIA/tpFiltrage/src/tp_filtrage.c
@@ -3,15 +3,33 @@
 #include <it/parser.h>
 
 
-void filtrage_monodimensionnel(imat & image, const imat & filtre) {
-    imat tmp = imat_clone(image);
-    for (int i=0; i<imat_height(image); i++)
-        for (int j=0; j<imat_width(image); j++) {
+void filtrage_monodimensionnel(imat * image, const imat * filtre) {
+    int i, j, j_f;
+    imat tmp = imat_clone(*image);
+    int offset = imat_width(*filtre)/2;
+
+    int coeff = 0;
+    for (j=0; j<imat_width(*filtre); j++)
+        coeff+=*filtre[0][j];
+
+    for (i=0; i<imat_height(*image); i++)
+        for (j=offset; j<imat_width(*image)-offset; j++) {
             int value = 0;
-            for (int j_f=0; j_f<imat_width(filtre); j_f++)
-                value+=tmp[i][j+j_f-(imat_width(filtre)/2)] * filtre[0][j_f];
-            image[i][j] = value;
+            for (j_f=0; j_f<imat_width(*filtre); j_f++) {
+                value+=tmp[i][j+j_f-offset] * *filtre[0][j_f];
+            }
+            *image[i][j] = value / coeff;
         }
+
+    imat_delete(tmp);
+}
+
+void filtrage_bidimensionnel(imat * image, const imat * filtre) {
+    filtrage_monodimensionnel(image, filtre);
+    imat transpose = imat_new_transpose(*image);
+    filtrage_monodimensionnel(&transpose, filtre);
+    *image = imat_new_transpose(transpose);
+    imat_delete(transpose);
 }
 
 int main( int argc, char ** argv )
@@ -47,6 +65,13 @@ int main( int argc, char ** argv )
   // filtrage bidimensionnel d'une image avec un filtre separable
   //---------------------------------------------------//
 
+  mat gauss          = parser_get_imat( parser, "gaussien" );
+  imat res = imat_clone(im);
+
+  filtrage_bidimensionnel(&res,&gauss);
+
+
+  imat_pgm_write("gaussien.pgm",im);
 
   //---------------------------------------------------//
   // filtrage d'une image avec un filtre moyenneur
