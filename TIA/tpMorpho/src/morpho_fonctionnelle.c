2d3ffbaade91236aaa9ab81a9d016acf3b8763ce
Fred <zokta@Arcktick.(none)>
2014-12-19 10:18:16 +0100

    TPMORPHO

--- TIA/tpMorpho/src/morpho_fonctionnelle.c
+++ TIA/tpMorpho/src/morpho_fonctionnelle.c
@@ -11,9 +11,21 @@
 // par rapport a l'element structurant masque
 
 int applique_masque_dilatation_NDG( imat im, imat masque, int i, int j){
-
-  // A COMPLETER
-  return 0;
+  int a,b,max;
+  max=0;
+  int deltaH=imat_height(masque)/2;
+  int deltaW=imat_width(masque)/2;
+  for(a=-deltaH;a<=deltaH;a++){
+    for(b=-deltaW;b<=deltaW;b++){
+        if(pasDepassement(im,i+a,j+b)) {	
+            if(1==masque[a+deltaH][b+deltaW])
+			    if(im[i+a][j+b]>max)
+			        max=im[i+a][j+b];
+		}
+	}
+  }
+  
+  return max;
 }
 
 // Cette fonction calcule la dilatation en NDG de l'image im par l'element structurant
@@ -21,9 +33,14 @@ int applique_masque_dilatation_NDG( imat im, imat masque, int i, int j){
 
 imat dilatation_NDG( imat im, imat masque ){
 
-  // A COMPLETER
-
-  return imat_new_zeros(1,1);
+  imat res = imat_clone(im);
+  int i,j;
+	for(i=0;i<imat_height(im);i++) {
+		for(j=0;j<imat_width(im);j++) {
+			res[i][j]=applique_masque_dilatation_NDG(im, masque, i, j)*255;
+		}
+	}
+  return res;
 }
 
 // Cette fonction calcule l'erosion en NDG du pixel (i,j) de l'image im
@@ -31,10 +48,20 @@ imat dilatation_NDG( imat im, imat masque ){
 
 int applique_masque_erosion_NDG( imat im, imat masque, int i, int j){
 
-
-  // A COMPLETER
-
-  return 0;
+  int a,b,min;
+  min=255;
+  int deltaH=imat_height(masque)/2;
+  int deltaW=imat_width(masque)/2;
+  for(a=-deltaH;a<=deltaH;a++){
+        for(b=-deltaW;b<=deltaW;b++){
+		if(pasDepassement(im,i+a,j+b)) {
+			if(masque[a+deltaH][b+deltaW]==1)
+				if(im[i+a][j+b]<min)
+				    min=im[i+a][j+b];
+		}
+	}
+  }
+  return min;
 }
 
 // Cette fonction calcule l'erosion en NDG de l'image im par l'element structurant
@@ -42,9 +69,13 @@ int applique_masque_erosion_NDG( imat im, imat masque, int i, int j){
 
 imat erosion_NDG( imat im, imat masque ){
 
- // A COMPLETER
-
-  return imat_new_zeros(1,1);
+  imat res = imat_clone(im);
+  int i,j;
+	for(i=0;i<imat_height(im);i++) {
+		for(j=0;j<imat_width(im);j++) {
+			res[i][j]=applique_masque_erosion_NDG(im, masque, i, j)*255;
+		}
+	}
+  return res;
 }
-
 
